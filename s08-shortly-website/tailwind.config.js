11718088705d4e638b386b169a938453ad2c7ed6
Rommel Rico <me@rommelrico.com>
2024-04-07 13:07:09 -0700

    :wrench: s8: Custom fonts

--- s08-shortly-website/tailwind.config.js
+++ s08-shortly-website/tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
         grayishViolet: 'hsl(257, 7%, 63%)',
         veryDarkBlue: 'hsl(255, 11%, 22%)',
         veryDarkViolet: 'hsl(260, 8%, 14%)',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      spacing: {
+        180: '32rem',
       }
     },
   },
