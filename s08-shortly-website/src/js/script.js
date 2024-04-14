602bfa94ce8978747f2af8eb75e9091eb274635e
Rommel Rico <me@rommelrico.com>
2024-04-14 14:17:26 -0700

    :sparkles: s8: Mobile Menu

--- s08-shortly-website/src/js/script.js
+++ s08-shortly-website/src/js/script.js
@@ -21,3 +21,15 @@ const formSubmit = (e) => {
 }
 
 linkForm.addEventListener('submit', formSubmit);
+
+
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+const navToggle = () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
+
+btn.addEventListener("click", navToggle);
