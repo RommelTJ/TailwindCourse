208b76160adea31e2e92dc403a5ee29f1affd791
Rommel Rico <me@rommelrico.com>
2024-04-16 07:40:50 -0700

    :wrench: s9: Custom screens

--- s09-testimonial-grid-website/tailwind.config.js
+++ s09-testimonial-grid-website/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  screens: {
+    sm: '480px',
+    md: '768px',
+    lg: '976px',
+    xl: '1440px',
+  },
   theme: {
     extend: {},
   },
