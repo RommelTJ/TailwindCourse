a915bff10a3552298a1b7af2e9c0b40560f5cf1e
Rommel Rico <me@rommelrico.com>
2024-04-19 20:06:41 -0700

    :wrench: s10: Colors

--- s10-fylo-website/tailwind.config.js
+++ s10-fylo-website/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: 'hsl(217, 28%, 15%)',
+        darkBlue1: 'hsl(218, 28%, 13%)',
+        darkBlue2: 'hsl(216, 53%, 9%)',
+        darkBlue3: 'hsl(219, 30%, 18%)',
+        accentCyan: 'hsl(176, 68%, 64%)',
+        accentBlue: 'hsl(198, 60%, 50%)',
+        lightRed: 'hsl(0, 100%, 63%)'
+      }
+    },
   },
   plugins: [],
 }
