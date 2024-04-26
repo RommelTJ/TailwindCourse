864ed7bcc32711f97cfc804696d0f6a945cc6ba4
Rommel Rico <me@rommelrico.com>
2024-04-26 12:12:01 -0700

    :wrench: s11: Colors

--- s11-bookmark-manager-website/tailwind.config.js
+++ s11-bookmark-manager-website/tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
         md: "768px",
         lg: "1020px",
         xl: "1440px",
+      },
+      colors: {
+        softBlue: 'hsl(231, 69%, 60%)',
+        softRed: 'hsl(0, 94%, 66%)',
+        grayishBlue: 'hsl(229, 8%, 60%)',
+        veryDarkBlue: 'hsl(229, 31%, 21%)',
       }
     },
   },
