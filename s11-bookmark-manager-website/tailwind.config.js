c9639c1559469b1b237cf6be1ca04ab6e657451e
Rommel Rico <me@rommelrico.com>
2024-04-26 12:10:42 -0700

    :wrench: s11: Screen configs

--- s11-bookmark-manager-website/tailwind.config.js
+++ s11-bookmark-manager-website/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1020px",
+        xl: "1440px",
+      }
+    },
   },
   plugins: [],
 }
