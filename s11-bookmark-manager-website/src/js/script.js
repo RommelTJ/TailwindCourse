684ddecdb565fffd165b9349bd6ab30d75cba78f
Rommel Rico <me@rommelrico.com>
2024-04-30 07:59:01 -0700

    :sparkles: s11: Mobile Menu

--- s11-bookmark-manager-website/src/js/script.js
+++ s11-bookmark-manager-website/src/js/script.js
@@ -1,6 +1,10 @@
 const tabs = document.getElementsByClassName("tab");
 const panels = document.getElementsByClassName("panel");
 
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
+
 const onTabClick = (e) => {
   // Remove the active class from all the tabs
   for (let i = 0; i < tabs.length; i++) {
@@ -23,3 +27,18 @@ for (let i = 0; i < tabs.length; i++) {
   const tab = tabs[i];
   tab.addEventListener("click", onTabClick);
 }
+
+// Hamburger Menu'
+const navToggle = () => {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg');
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg');
+  }
+}
+
+btn.addEventListener('click', navToggle);
